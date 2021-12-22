dc7bffd8f3b13789fe0cacbadf48cb441dafcb70
danmartin98 <danmartin98@icloud.com>
2021-12-22 16:25:03 +0000

    Tidied up Kata

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 /* 
 👉 Write your kata here!
 */
-// It takes in three parameters  which is your firstname, lastname and age. It creates a username. The user name is created with the first letter of your first, a dot and then your surname and your age straight after. The first letter in the username has to be lowercase and the remaining letters are uppercase.
-// if it is a double barrell name it keeps in the dash.
 
-// For example
-// (Dan-martin, martin, 45)  => (dMARTIN45)
-// (kaz, MART-WER,80) => (kMART-WER80)
+// You have been assigned to create usernames for the people in your company 
+// The format the company uses is:
+// -- First initial of first name 
+// -- Surname 
+// -- Age
+// For Example, Dan Martin who is 23, will have the username : dMARTIN23
+// The function takes in three parameters: firstname, lastname and age and should create the username based on these paramaters. 
+
+// If it is a double barrell name it keeps in the dash.
+// For example, Kazeem George-Ryan who is 45, will have the username : kGEORGE-RYAN45
+
+// PLEASE NOTE, USERNAMES ARE CASE SENSITIVE
+
+
 
 //👉 Write the function your CodeWarriors will start with below here:
+
 export function createUsername(firstname, surname, age) {
   const usernameFirstLetter = firstname[0].toLowerCase();
   const usernameSurname = surname.toUpperCase();
--- main.test.js
+++ main.test.js
@@ -1,13 +1,7 @@
-// const { TestWatcher } = require("jest")
 import { createUsername } from "./main.js";
 
 //👉 Write your tests below here:
-// It takes in three parameters  which is your firstname, lastname and age. It creates a username. The user name is created with the first letter of your first, a dot and then your surname and your age straight after. The first letter in the username has to be lowercase and the remaining letters are uppercase.
-// if it is a double barrell name it keeps in the dash.
 
-// For example
-// (Dan-martin, martin, 45)  => (dMARTIN45)
-// (kaz, MART-WER,80) => (kMART-WER80)
 
 describe(`Username Function for Kata`, function () {
   test(`Should create a username out of the 3 paramaters given. For example (Dan, martin, 23) => dMARTIN23`, function () {
