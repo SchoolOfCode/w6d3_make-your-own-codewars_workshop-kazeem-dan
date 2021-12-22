c8e1562fd839ed6c39beb09c35bc413f627169f2
cazmaar <kazeem.alatishe@yahoo.com>
2021-12-22 16:13:44 +0000

    function created

--- main.test.js
+++ main.test.js
@@ -14,16 +14,22 @@ describe(`Username Function for Kata`, function () {
     const firstname = "Dan";
     const surname = "Martin";
     const age = 23;
-    const expected = "dMARTIN23";
+    const expected =
+      firstname[0].toLowerCase() + surname.toUpperCase() + Number(age);
+    console.log(expected);
     const actual = createUsername(firstname, surname, age);
-    expect(actual).toBe(expected);
+    expect(actual).toEqual(expected);
   });
   test(`Should create a username out of the 3 paramaters given. For example (Dan, martin, 23) => dMARTIN23`, function () {
     const firstname = "Bob";
     const surname = "kazeem-mali";
     const age = 34;
-    const expected = "bKAZEEM-MALI34";
+    const expected =
+      String(firstname[0].toLowerCase()) +
+      String(surname.toUpperCase()) +
+      Number(age);
     const actual = createUsername(firstname, surname, age);
     expect(actual).toBe(expected);
   });
 });
+
